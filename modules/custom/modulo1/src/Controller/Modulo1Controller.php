35aeab7ad081efb07ec7f60e25bb5ff3f11e28b7
Jhonatanfdez <jhonatandavidfernandezr@gmail.com>
2022-01-21 17:12:12 -0400

    Crear librería en Drupal (css y js)

--- modules/custom/modulo1/src/Controller/Modulo1Controller.php
+++ modules/custom/modulo1/src/Controller/Modulo1Controller.php
@@ -7,13 +7,21 @@ use Drupal\Core\Controller\ControllerBase;
 class Modulo1Controller extends ControllerBase {
 
     public function content() {
-
+        
         \Drupal::service('page_cache_kill_switch')->trigger(); 
 
-        return [
+        $contenido =[];
+
+        $contenido[]= [
+           '#markup' => '
+                <h2>Prueba 7</h2> 
+                notificaciones: <span class="badge bg-success colorrojo">7</span>
            
-            '#markup' => '<h2>Prueba 6</h2>'
+           '
         ];
+
+        $contenido['#attached']['library'][] = 'modulo1/global';
+        return $contenido;
 
     }
 
